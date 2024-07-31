1b7b452d283307c1c1a0fc09e86258432b13ce76
Nannita1403 <nannimagi@gmail.com>
2024-07-31 14:20:07 +0200

    create Routes

--- React_Router/src/App.jsx
+++ React_Router/src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
 
 const App = () => {
 
