ff1b12d20f76ff4973d6cae1adcac4d5f87cf258
Nannita1403 <nannimagi@gmail.com>
2024-07-31 13:53:41 +0200

    Create Header

---
+++ React_Router/src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/" activeclassname="active">
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" activeclassname="active">
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" activeclassname="active">
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
